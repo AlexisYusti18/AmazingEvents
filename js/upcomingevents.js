ea70e3655263aee406ea84b5cc0f34f198a5adf6
Alexis Yusti <alexisyusti18@gmail.com>
2022-08-08 21:16:24 -0300

    Amazing Events

--- js/upcomingevents.js
+++ js/upcomingevents.js
@@ -38,7 +38,7 @@ const nuevosCheckbox=()=>{
     var imputCheckbox="" 
     categorias.forEach(category=>{     
         imputCheckbox+=`
-        <label><input class="busqueda-check" type="radio" value="${category}"> ${category}</label> `
+        <label><input class="busqueda-check" type="checkbox" value="${category}"> ${category}</label> `
     })
     checkboxes.innerHTML=imputCheckbox 
 
