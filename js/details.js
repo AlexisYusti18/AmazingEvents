885fd943cac5f5a79a08d832950710d92328ece5
AlexisYusti18 <alexisyusti18@gmail.com>
2023-04-01 17:19:36 -0300

    nueva api

--- js/details.js
+++ js/details.js
@@ -1,7 +1,7 @@
 let eventos=[]
 
 async function cardsId(){
-       await fetch('https://amazing-events.herokuapp.com/api/events')
+       await fetch('https://mindhub-xj03.onrender.com/api/amazing')
         .then(response=>response.json())
         .then(json=>eventos=json)
         
